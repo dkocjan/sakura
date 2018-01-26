46f66bd5edbbff5fd8eb8401cfcbf70fdf9a7eeb
dkocjan <dkocjan123@gmail.com>
2018-01-26 15:39:10 +0100

    Add basic typography for headings and p, update readme.md, include Muli font from google fonts, add watch task

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 ## Sakura 🌸
 Small, fast and responsive CSS framework 🌸
+
+
+## Features (in development)
+##### Responsive grid
+##### Typography
+##### Buttons
+##### Forms and inputs
+##### Icons
+##### Cards
+
+## How to use
+#### Production
+1. Download Sakura: `/dist/sakura.min.css` (or `sakura.gz` if preferred)
+2. Include Sakura:`<link rel="stylesheet" src="./sakura.min.css/>` 
+3. Include Font: `<link href="https://fonts.googleapis.com/css?family=Muli:400,700" rel="stylesheet">`
+
+##### Development
+`yarn start` - default gulp task, builds css to `/dist` in development, uses sourcemaps
+
+`yarn watch` - opens localhost with `/index.html` and watches for changes
+
+`yarn build` - builds css to `/dist` for production, minified and gzipped
--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,31 @@ const postcss = require('gulp-postcss'),
   cleancss = require('gulp-clean-css'),
   gzip = require('gulp-gzip'),
   autoprefixer = require('gulp-autoprefixer'),
-  sourcemaps = require('gulp-sourcemaps');
+  sourcemaps = require('gulp-sourcemaps'),
+      browserSync = require('browser-sync'),
+rename = require('gulp-rename');
 
 // Dev task
-gulp.task('default', () =>
+gulp.task('dev', () =>
   gulp
     .src('src/sakura.css')
     .pipe(postcss([next()]))
     .pipe(sourcemaps.init())
-    .pipe(cleancss({ level: 2 }))
+    .pipe(cleancss())
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('dist/'))
+    .pipe(browserSync.stream())
 );
 
-// todo: Dev watch task (browser sync)
+// Dev watch task (browser sync)
+gulp.task('reload', () => browserSync.reload());
+gulp.task('watch', ['dev'], () => {
+  browserSync({ server: true }, function(err, bs) {
+    console.log(bs.options.getIn(["urls", "local"]));
+  });
+  gulp.watch(['index.html'], ['reload']);
+  gulp.watch(['src/sakura.css'], ['dev']);
+});
 
 // Build task
 gulp.task('build', () =>
@@ -27,7 +38,15 @@ gulp.task('build', () =>
     .pipe(postcss([next()]))
     .pipe(autoprefixer({ browsers: ['last 10 versions', '> 1%'], cascade: false }))
     .pipe(cleancss({ level: 2 }))
+    .pipe(rename({
+      basename: 'sakura.min',
+      extname: '.css'
+    }))
     .pipe(gulp.dest('dist/'))
     .pipe(gzip())
+    .pipe(rename({
+      basename: 'sakura',
+      extname: '.gz'
+    }))
     .pipe(gulp.dest('dist/'))
 );
